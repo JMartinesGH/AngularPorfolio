e860460e47cba6735322585f100fa8c1b582de92
JMartinesGH <martines.jeff@gmail.com>
2013-10-18 16:28:28 -0400

    removed references to my unique hello world

--- js/controllers.js
+++ js/controllers.js
@@ -4,7 +4,6 @@ function workCtrl($scope, $http){
 	$http.get('work.json').success(function(data) {
     	$scope.works = data;
  	});
-	
-	$scope.hello = "Hello, Buttface!";
+
 	$scope.orderProp = '-age';
 }
