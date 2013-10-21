5467bf895444c9138c0504a026287b8e5ba16476
JMartinesGH <martines.jeff@gmail.com>
2013-10-21 12:00:00 -0400

    first route

--- js/app.js
+++ js/app.js
@@ -1,2 +1,8 @@
 'use strict';
 
+angular.module('portfolio', []).
+  config(['$routeProvider',function($routeProvider){
+  	$routeProvider.
+  	  when('/', {templateUrl: 'partials/fullDetail.html', controller: workCtrl}).
+  	  otherwise({redirectTo: '/all'});	
+}]);
